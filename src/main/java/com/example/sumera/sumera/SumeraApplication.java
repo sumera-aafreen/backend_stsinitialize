e045b41951b7a0a43a727eae35bb972feebadf71
sumera-aafreen <727723euai125@skcet.ac.in>
2025-09-17 09:54:48 +0530

    added a single line in java file

--- src/main/java/com/example/sumera/sumera/SumeraApplication.java
+++ src/main/java/com/example/sumera/sumera/SumeraApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SumeraApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SumeraApplication.class, args);
+		System.out.println("Sumera aafreen");
 	}
 
 }
