60b942589e869c77010c3ebf0ef60c80697c0642
Sumera <sumerayesdhan@gmail.com>
2025-09-17 09:11:18 +0530

    initial setup

---
+++ src/test/java/com/example/sumera/sumera/SumeraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sumera.sumera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumeraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
